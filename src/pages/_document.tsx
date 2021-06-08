979f7a19a25009fb828fb7090ee33594430cc428
leonardo <leojn8@gmail.com>
2021-06-08 02:20:36 -0300

    atual

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,12 +5,17 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
-        
+          <link rel="manifest" href="/manifest.json" />
+          <link rel="icon" href="/favicon.png" />
           <link rel="shortcut icon" href="favicon.png" type="image/png" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap"
             rel="stylesheet"
+          />
+          <meta
+            name="viewport"
+            content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
           />
         </Head>
         <body>
